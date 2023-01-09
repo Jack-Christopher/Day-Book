6d3aff52823e177a8abd9307f8f81427714efd38
Jack Christopher <jhuaihuah@unsa.edu.pe>
2023-01-09 14:37:02 -0500

    feat: Customized the Menubar and created a Settings window

--- main.js
+++ main.js
@@ -1,22 +1,97 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain  } = require('electron')
 const path = require('path')
-const cipher = require('./cipher.js')
 
+
+let mainWindow;
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
     width: 800,
     height: 500,
     icon: path.join(__dirname, 'logo.png'),
+    // autoHideMenuBar: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
+//   mainWindow.show();
 }
+
+
+function createChildWindow() {
+    childWindow = new BrowserWindow({
+      width: 600,
+      height: 360,
+      icon: path.join(__dirname, 'logo.png'),
+      modal: true,
+      show: false,
+      parent: mainWindow, // Make sure to add parent window here
+      autoHideMenuBar: true,
+    
+      // Make sure to add webPreferences with below configuration
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        enableRemoteModule: true,
+      },
+    });
+    
+    // Child window loads settings.html file
+    childWindow.loadFile("settings.html");
+    
+    childWindow.once("ready-to-show", () => {
+      childWindow.show();
+    });
+}
+
+
+const template = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Show Notes',
+                click: () => console.log('Show Notes')
+            },
+            {
+                label: 'Exit',
+                click: () => app.quit()
+            }
+        ]
+    },
+    {
+       label: 'View',
+       submenu: [
+          {
+             role: 'reload'
+          },
+          {
+             role: 'toggledevtools'
+          }
+       ]
+    },
+
+    {
+        label: 'Settings',
+            submenu: [
+                {
+                    label: 'Settings',
+                    click: () => createChildWindow()
+                }
+            ]
+    }
+ ]
+ 
+ const menu = Menu.buildFromTemplate(template)
+ Menu.setApplicationMenu(menu)
+
+
 
 app.whenReady().then(() => {
   createWindow()
